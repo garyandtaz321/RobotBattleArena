1583df6d12d30d8d2787318e9c08c060a65861d3
Meh Man <Meh Man>
2016-11-05 12:24:07 -0400

    wheeeee

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -1,7 +1,6 @@
 var Player = require('../models/player');
 
 var Game = function () {
-  this.testentity = null;
 };
 
 module.exports = Game;
@@ -9,32 +8,52 @@ module.exports = Game;
 Game.prototype = {
 
   create: function () {
-    var x = (this.game.width / 2) - 100;
-    var y = (this.game.height / 2) - 50;
+    this.game.world.setBounds(0, 0, 1600, 1600);
+    this.physics.startSystem(Phaser.Physics.ARCADE);
 
-    this.testentity = new Player(this.game, x, y);
-    this.testentity.anchor.setTo(0.5, 0.5);
+    this.map = this.add.tilemap('arena1');
+    this.map.addTilesetImage('CaveStory-MimigaVillage', 'CaveStory-MimigaVillage');
+    this.map.addTilesetImage('lava', 'lava');
+    this.map.addTilesetImage('swankymemes', 'swankymemes');
 
-    this.input.onDown.add(this.onInputDown, this);
+    this.layer = this.map.createLayer('Tile Layer 1');
+    
+    this.map.setCollisionBetween(2043, 2104);
+    this.map.setTileIndexCallback(3605, () => {
+      console.log("Lava! Watch out!");
+      return;
+    }, this);
+
+    player = this.add.sprite(32, 32, 'player');
+    this.physics.arcade.enable(player);
+
+    this.game.camera.follow(player);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
   },
 
   update: function () {
-    var x, y, cx, cy, dx, dy, angle, scale;
+    this.physics.arcade.collide(player, this.layer);
 
-    x = this.input.position.x;
-    y = this.input.position.y;
-    cx = this.world.centerX;
-    cy = this.world.centerY;
-
-    angle = Math.atan2(y - cy, x - cx) * (180 / Math.PI);
-    this.testentity.angle = angle;
-
-    dx = x - cx;
-    dy = y - cy;
-    scale = Math.sqrt(dx * dx + dy * dy) / 100;
-
-    this.testentity.scale.x = scale * 0.6;
-    this.testentity.scale.y = scale * 0.6;
+    if (this.cursors.left.isDown) {
+          player.body.velocity.x = -100;
+      }
+      else if (this.cursors.right.isDown) {
+          player.body.velocity.x = 100;
+      }
+      else {
+          player.body.velocity.x = 0;
+      }
+      if (this.cursors.up.isDown){
+          player.body.velocity.y = -100;
+      }
+      else if (this.cursors.down.isDown){
+          player.body.velocity.y = 100;
+      }
+      else {
+          player.body.velocity.y = 0;
+      }
   },
 
   onInputDown: function () {
--- src/js/states/preloader.js
+++ src/js/states/preloader.js
@@ -13,7 +13,11 @@ Preloader.prototype = {
 
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
     this.load.setPreloadSprite(this.asset);
-    this.load.image('testsprite', 'assets/test.png');
+    this.load.tilemap('arena1', 'assets/tilemap/arena1.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('CaveStory-MimigaVillage', 'assets/tilemap/CaveStory-MimigaVillage.png');
+    this.load.image('lava', 'assets/tilemap/lava.png');
+    this.load.image('swankymemes', 'assets/tilemap/swankymemes.png');
+    this.load.image('player', 'assets/car.png');
   },
 
   create: function () {
@@ -22,7 +26,7 @@ Preloader.prototype = {
 
   update: function () {
     if (!!this.ready) {
-      this.game.state.start('Menu');
+      this.game.state.start('Game');
     }
   },
 
