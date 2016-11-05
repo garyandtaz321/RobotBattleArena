7d6c32718c5997a4d968b8c415aa2405d85897df
Meh Man <Meh Man>
2016-11-05 14:22:51 -0400

    working on invincibility

--- src/js/states/preloader.js
+++ src/js/states/preloader.js
@@ -17,6 +17,7 @@ Preloader.prototype = {
     this.load.image('CaveStory-MimigaVillage', 'assets/tilemap/CaveStory-MimigaVillage.png');
     this.load.image('lava', 'assets/tilemap/lava.png');
     this.load.image('swankymemes', 'assets/tilemap/swankymemes.png');
+    this.load.image('swankymemes2', 'assets/tilemap/swankymemes2.png');
     this.load.image('player', 'assets/car.png');
   },
 
